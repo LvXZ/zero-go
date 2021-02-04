3d1ae8403961b4d58d0c7a549fd95b368894f228
lvxiaozheng <lvxiaozheng@bilibili.com>
2021-02-04 12:11:52 +0800

    表单提交

--- my-gin-demo/main.go
+++ my-gin-demo/main.go
@@ -2,10 +2,11 @@ package main
 
 // @Author: lvxiaozheng
 // @Date: 2021/2/4 10:52
-// @Description: 
+// @Description:
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strings"
 
@@ -51,6 +52,60 @@ func main() {
 		c.String(http.StatusOK, fmt.Sprintf("username:%s,password:%s,type:%s", username, password, types))
 	})
 
+	//文件上传
+	//限制上传最大尺寸
+	r.MaxMultipartMemory = 8 << 20
+	r.POST("/upload", func(c *gin.Context) {
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.String(500, "上传图片出错")
+		}
+		// c.JSON(200, gin.H{"message": file.Header.Context})
+		c.SaveUploadedFile(file, file.Filename)
+		c.String(http.StatusOK, file.Filename)
+	})
+
+	//上传图片
+	r.POST("/upload/image", func(c *gin.Context) {
+		_, headers, err := c.Request.FormFile("file")
+		if err != nil {
+			log.Printf("Error when try to get file: %v", err)
+		}
+		//headers.Size 获取文件大小
+		if headers.Size > 1024*1024*2 {
+			fmt.Println("文件太大了")
+			return
+		}
+		//headers.Header.Get("Content-Type")获取上传文件的类型
+		if headers.Header.Get("Content-Type") != "image/png" {
+			fmt.Println("只允许上传png图片")
+			c.String(http.StatusNotAcceptable, "只允许上传png图片")
+			return
+		}
+		c.SaveUploadedFile(headers, "/Users/lvxiaozheng/Documents/"+headers.Filename)
+		c.String(http.StatusOK, headers.Filename)
+	})
+
+	//上传多个文件
+	// 限制表单上传大小 8MB，默认为32MB
+	r.MaxMultipartMemory = 8 << 20
+	r.POST("/upload/files", func(c *gin.Context) {
+		form, err := c.MultipartForm()
+		if err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("get err %s", err.Error()))
+		}
+		// 获取所有文件
+		files := form.File["files"]
+		// 遍历所有文件
+		for _, file := range files {
+			// 逐个存储
+			if err := c.SaveUploadedFile(file, file.Filename); err != nil {
+				c.String(http.StatusBadRequest, fmt.Sprintf("upload err %s", err.Error()))
+				return
+			}
+		}
+		c.String(http.StatusOK, fmt.Sprintf("upload ok %d files", len(files)))
+	})
 
 	// 3.监听端口，默认在8080
 	// Run("里面不指定端口号默认为8080")
